99ddb39c504f1096072256a9bb523b6e76a4ee2c
Rafael Henrique Tibaes <r@fael.nl>
2017-01-28 13:47:40 -0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -7,103 +7,11 @@
 #include <vector>
 
 #include "ctrlPts.hpp"
+#include "shaders.hpp"
 
 using namespace std;
 
 const int WIN_SIZE = 500;
-
-// Read a shader source from a file
-// store the shader source in a std::vector<char>
-void read_shader_src(const char *fname, std::vector<char> &buffer);
-
-// Compile a shader
-GLuint load_and_compile_shader(const char *fname, GLenum shaderType);
-
-// Create a program from two shaders
-GLuint create_program(const char *path_vert_shader,
-                      const char *path_frag_shader);
-
-// Called when the window is resized
-void GLFWCALL window_resized(int width, int height);
-
-// Called for keyboard events
-void keyboard(int key, int action);
-
-// Called for mouse button
-void mouse(int button, int action);
-
-// Render scene
-void display(GLuint &vao);
-
-// Initialize the data to be rendered
-void initialize(GLuint &vao);
-
-int main() {
-  // Initialize GLFW
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW! I'm out!" << std::endl;
-    exit(-1);
-  }
-
-  // Use OpenGL 3.2 core profile
-  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
-
-  // Open a window and attach an OpenGL rendering context to the window surface
-  if (!glfwOpenWindow(WIN_SIZE, WIN_SIZE, 8, 8, 8, 0, 0, 0, GLFW_WINDOW)) {
-    std::cerr << "Failed to open a window! I'm out!" << std::endl;
-    glfwTerminate();
-    exit(-1);
-  }
-
-  // Register a callback function for window resize events
-  glfwSetWindowSizeCallback(window_resized);
-
-  // Register a callback function for keyboard pressed events
-  glfwSetKeyCallback(keyboard);
-
-  // Register a callback function for mouse events
-  glfwSetMouseButtonCallback(mouse);
-
-  // Print the OpenGL version
-  int major, minor, rev;
-  glfwGetGLVersion(&major, &minor, &rev);
-  std::cout << "OpenGL - " << major << "." << minor << "." << rev << std::endl;
-
-  // Initialize GLEW
-  glewExperimental = GL_TRUE;
-  if (glewInit() != GLEW_OK) {
-    std::cerr << "Failed to initialize GLEW! I'm out!" << std::endl;
-    glfwTerminate();
-    exit(-1);
-  }
-
-  // Create a vertex array object
-  GLuint vao;
-
-  // Initialize the data to be rendered
-  initialize(vao);
-
-  // Create a rendering loop
-  int running = GL_TRUE;
-
-  while (running) {
-    // Display scene
-    display(vao);
-
-    // Pool for events
-    glfwPollEvents();
-    // Check if the window was closed
-    running = glfwGetWindowParam(GLFW_OPENED);
-  }
-
-  // Terminate GLFW
-  glfwTerminate();
-
-  return 0;
-}
 
 // Render scene
 void display(GLuint &vao) {
@@ -179,79 +87,69 @@ void mouse(int button, int action) {
   }
 }
 
-// Read a shader source from a file
-// store the shader source in a std::vector<char>
-void read_shader_src(const char *fname, std::vector<char> &buffer) {
-  std::ifstream in;
-  in.open(fname, std::ios::binary);
-
-  if (in.is_open()) {
-    // Get the number of bytes stored in this file
-    in.seekg(0, std::ios::end);
-    size_t length = (size_t)in.tellg();
-
-    // Go to start of the file
-    in.seekg(0, std::ios::beg);
-
-    // Read the content of the file in a buffer
-    buffer.resize(length + 1);
-    in.read(&buffer[0], length);
-    in.close();
-    // Add a valid C - string end
-    buffer[length] = '\0';
-  } else {
-    std::cerr << "Unable to open " << fname << " I'm out!" << std::endl;
+int main() {
+  // Initialize GLFW
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW! I'm out!" << std::endl;
     exit(-1);
   }
-}
 
-// Compile a shader
-GLuint load_and_compile_shader(const char *fname, GLenum shaderType) {
-  // Load a shader from an external file
-  std::vector<char> buffer;
-  read_shader_src(fname, buffer);
-  const char *src = &buffer[0];
+  // Use OpenGL 3.2 core profile
+  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
 
-  // Compile the shader
-  GLuint shader = glCreateShader(shaderType);
-  glShaderSource(shader, 1, &src, NULL);
-  glCompileShader(shader);
-  // Check the result of the compilation
-  GLint test;
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &test);
-  if (!test) {
-    std::cerr << "Shader compilation failed with this message:" << std::endl;
-    std::vector<char> compilation_log(512);
-    glGetShaderInfoLog(shader, compilation_log.size(), NULL,
-                       &compilation_log[0]);
-    std::cerr << &compilation_log[0] << std::endl;
+  // Open a window and attach an OpenGL rendering context to the window surface
+  if (!glfwOpenWindow(WIN_SIZE, WIN_SIZE, 8, 8, 8, 0, 0, 0, GLFW_WINDOW)) {
+    std::cerr << "Failed to open a window! I'm out!" << std::endl;
     glfwTerminate();
     exit(-1);
   }
-  return shader;
-}
 
-// Create a program from two shaders
-GLuint create_program(const char *path_vert_shader,
-                      const char *path_frag_shader) {
-  // Load and compile the vertex and fragment shaders
-  GLuint vertexShader =
-      load_and_compile_shader(path_vert_shader, GL_VERTEX_SHADER);
-  GLuint fragmentShader =
-      load_and_compile_shader(path_frag_shader, GL_FRAGMENT_SHADER);
+  // Register a callback function for window resize events
+  glfwSetWindowSizeCallback(window_resized);
 
-  // Attach the above shader to a program
-  GLuint shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
+  // Register a callback function for keyboard pressed events
+  glfwSetKeyCallback(keyboard);
 
-  // Flag the shaders for deletion
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  // Register a callback function for mouse events
+  glfwSetMouseButtonCallback(mouse);
 
-  // Link and use the program
-  glLinkProgram(shaderProgram);
-  glUseProgram(shaderProgram);
+  // Print the OpenGL version
+  int major, minor, rev;
+  glfwGetGLVersion(&major, &minor, &rev);
+  std::cout << "OpenGL - " << major << "." << minor << "." << rev << std::endl;
 
-  return shaderProgram;
+  // Initialize GLEW
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Failed to initialize GLEW! I'm out!" << std::endl;
+    glfwTerminate();
+    exit(-1);
+  }
+
+  // Create a vertex array object
+  GLuint vao;
+
+  // Initialize the data to be rendered
+  initialize(vao);
+
+  // Create a rendering loop
+  int running = GL_TRUE;
+
+  while (running) {
+    // Display scene
+    display(vao);
+
+    // Pool for events
+    glfwPollEvents();
+    // Check if the window was closed
+    running = glfwGetWindowParam(GLFW_OPENED);
+  }
+
+  // Terminate GLFW
+  glfwTerminate();
+
+  return 0;
 }
