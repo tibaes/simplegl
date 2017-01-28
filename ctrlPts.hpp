6d61b854bdf12ffb007035ea27fe47cef0a0f1be
Rafael Henrique Tibaes <r@fael.nl>
2017-01-28 14:22:09 -0300

    refactor control points

--- ctrlPts.hpp
+++ ctrlPts.hpp
@@ -4,20 +4,24 @@
 #include <vector>
 using namespace std;
 
+using Point2d = struct { float x, y; };
+using Point3d = struct { float x, y, z; };
+
 class CtrlPts {
 public:
-  using Point2d = struct { int x, y; };
   CtrlPts() = default;
   ~CtrlPts() = default;
+  void pushCtrlPt(Point2d p) { v.push_back(p); }
+  void popCtrlPt() { v.pop_back(); }
+  vector<Point2d> getRawCtrlPts() const { return v; }
 
-  void pushCtrlPt(Point2d p);
-  void popCtrlPt();
-  vector<Point2d> getRawCtrlPts() const;
-  vector<Point2d> upsample(int min_points) const;
+  vector<Point2d> computeCurve(int min_points) const;
 
 private:
   vector<Point2d> v;
 };
+
+vector<Point3d> computeModel(vector<Point2d> curve);
 
 auto ctrlPts = CtrlPts();
 
