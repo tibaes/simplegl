fdcfdaac36e1d7c2d86ede2e8f6de50df996091b
Rafael Henrique Tibaes <r@fael.nl>
2017-02-04 16:57:29 -0300

    update headers in rendering protocol

--- RenderingProtocol.hpp
+++ RenderingProtocol.hpp
@@ -1,6 +1,20 @@
 #ifndef H_RenderingProtocol
 #define H_RenderingProtocol
 
+#include <GL/glew.h>
+#include <GL/glfw.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <vector>
+
 namespace cg {
 class RenderingProtocol {
 public:
