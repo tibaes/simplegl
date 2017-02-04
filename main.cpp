d1aba82c9bd5bfa4ca3ea490b831e31de649dac3
Rafael Henrique Tibaes <r@fael.nl>
2017-02-04 15:10:15 -0300

    add glm headers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <GL/glew.h>
 #include <GL/glfw.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
