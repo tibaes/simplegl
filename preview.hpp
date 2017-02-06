41bd5b1c710e1129a3bed41f2c97864e4de0d85e
Rafael Henrique Tibaes <r@fael.nl>
2017-02-06 20:50:16 -0300

    display mirrored points

--- preview.hpp
+++ preview.hpp
@@ -16,53 +16,55 @@ class Preview : public RenderingProtocol,
 private:
   GLuint curveShader;
   GLuint linesVBO, linesVBA;
-  GLuint pointsVBO[2];
+  GLuint pointsVBO, pointsVBA;
   GLuint curveVBO[2];
   int rsz_ctrlPts{0}, rsz_smooth{0};
   vector<cg::Point2d> ctrlPts;
 
   void createCtrlPts() {
-    float color = 0.0f;
-    const float color_step = 1.0f / ctrlPts.size();
-    vector<float> vtx_points, vtx_points_color;
+    rsz_ctrlPts = ctrlPts.size();
+
+    GLfloat color = 0.0f;
+    const GLfloat color_step = 1.0f / ctrlPts.size();
+    vector<GLfloat> vtx_points, vtx_points_color;
     for (const auto p : ctrlPts) {
-      vtx_points.push_back(p.x);
-      vtx_points.push_back(p.y);
+      vtx_points.push_back(GLfloat(p.x));
+      vtx_points.push_back(GLfloat(p.y));
       vtx_points_color.push_back(color);
       vtx_points_color.push_back(1.0f);
       vtx_points_color.push_back(1.0f - color);
-      vtx_points_color.push_back(1.0f);
       color += color_step;
     }
-    rsz_ctrlPts = vtx_points.size();
+    auto sz_vertexes = vtx_points.size() * sizeof(GLfloat);
+    auto sz_colors = vtx_points_color.size() * sizeof(GLfloat);
 
-    glGenBuffers(2, pointsVBO);
+    glGenVertexArrays(1, &pointsVBA);
+    glBindVertexArray(pointsVBA);
 
-    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[0]);
-    glBufferData(GL_ARRAY_BUFFER, vtx_points.size() * sizeof(float),
-                 vtx_points.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[1]);
-    glBufferData(GL_ARRAY_BUFFER, vtx_points_color.size() * sizeof(float),
-                 vtx_points_color.data(), GL_STATIC_DRAW);
+    glGenBuffers(1, &pointsVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
+    glBufferData(GL_ARRAY_BUFFER, sz_vertexes + sz_colors, NULL,
+                 GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sz_vertexes, vtx_points.data());
+    glBufferSubData(GL_ARRAY_BUFFER, sz_vertexes, sz_colors,
+                    vtx_points_color.data());
+
+    GLint attrP = glGetAttribLocation(curveShader, "aPosition");
+    glVertexAttribPointer(attrP, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(attrP);
+
+    GLint attrC = glGetAttribLocation(curveShader, "aColor");
+    glVertexAttribPointer(attrC, 3, GL_FLOAT, GL_FALSE, 0,
+                          (GLvoid *)sz_vertexes);
+    glEnableVertexAttribArray(attrC);
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    glPointSize(3.0f);
   }
 
   void displayCtrlPts() {
-    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[0]);
-    int attrV = glGetAttribLocation(curveShader, "aPosition");
-    glVertexAttribPointer(attrV, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(attrV);
-
-    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[1]);
-    int attrC = glGetAttribLocation(curveShader, "aColor");
-    glVertexAttribPointer(attrC, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(attrC);
-
-    glPointSize(3.0f);
+    glBindVertexArray(pointsVBA);
     glDrawArrays(GL_POINTS, 0, rsz_ctrlPts);
-    glDisableVertexAttribArray(attrV);
-    glDisableVertexAttribArray(attrC);
-
-    // glEnable(GL_PROGRAM_POINT_SIZE);
   }
 
   void createCurve() {
@@ -73,7 +75,7 @@ private:
     vector<float> vtx_lines, vtx_lines_color;
     for (const auto p : smooth) {
       vtx_lines.push_back(p.x);
-      vtx_lines.push_back(p.y);
+      vtx_lines.push_back(-1 * p.y);
       vtx_lines_color.push_back(color);
       vtx_lines_color.push_back(1.0f);
       vtx_lines_color.push_back(1.0f - color);
@@ -137,21 +139,14 @@ public:
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(1.0, 0.0, 0.0, 0.0);
 
-    // glm::mat4 MVP = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
-
-    // glUseProgram(curveShader);
-    // int loc = glGetUniformLocation(curveShader, "uMVP");
-    // glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(MVP));
-    // displayCtrlPts();
     displayLines();
+    if (rsz_ctrlPts > 0)
+      displayCtrlPts();
 
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
     glfwSwapBuffers();
   }
 
-  void update() override {
-    // createCtrlPts();
-  }
+  void update() override { createCtrlPts(); }
 
   void onMouseClick(float x, float y) override {
     cout << "Adding control point: " << x << ", " << y << endl;
