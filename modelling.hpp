4cdfee4e67d7150879eb260cafe3367b9d8169f7
Rafael Henrique Tibaes <r@fael.nl>
2017-02-09 18:28:08 -0300

    base model rendering

--- modelling.hpp
+++ modelling.hpp
@@ -15,7 +15,58 @@ class Modelling : public RenderingProtocol,
                   public enable_shared_from_this<Modelling> {
 private:
   GLuint modelShader;
-  vector<Point3d> obj;
+  GLuint objVBO, objVBA;
+  uint trianglesObj{0};
+
+  void createObj(vector<cg::Point2d> ctrlPts) {
+    auto contour = cg::casteljau(ctrlPts);
+    auto obj = cg::model(contour);
+
+    auto md_h = contour.size();
+    auto md_w = obj.size() / md_h;
+    auto getVTX = [&](int h, int w) { return obj[h * md_w + (w % md_w)]; };
+
+    vector<Point3d> md_v;
+    for (auto h = 0; h < md_h - 1; ++h) {
+      for (auto w = 0; w < md_w; ++w) {
+        // triangle 1
+        md_v.push_back(getVTX(h, w));
+        md_v.push_back(getVTX(h + 1, w));
+        md_v.push_back(getVTX(h, w + 1));
+        // triangle 2
+        md_v.push_back(getVTX(h + 1, w));
+        md_v.push_back(getVTX(h + 1, w + 1));
+        md_v.push_back(getVTX(h, w + 1));
+      }
+    }
+
+    vector<GLfloat> vertexes;
+    for (auto p : md_v) {
+      vertexes.push_back(p.x);
+      vertexes.push_back(p.y);
+      vertexes.push_back(p.z);
+    }
+    trianglesObj = vertexes.size() / 3;
+
+    glGenVertexArrays(1, &objVBA);
+    glBindVertexArray(objVBA);
+
+    glGenBuffers(1, &objVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, objVBO);
+    glBufferData(GL_ARRAY_BUFFER, vertexes.size() * sizeof(GLfloat),
+                 vertexes.data(), GL_STATIC_DRAW);
+
+    GLint attrP = glGetAttribLocation(modelShader, "aPosition");
+    glVertexAttribPointer(attrP, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(attrP);
+
+    cout << "model has " << trianglesObj << " triangles" << endl;
+  }
+
+  void displayObj() {
+    glBindVertexArray(objVBA);
+    glDrawArrays(GL_TRIANGLES, 0, trianglesObj);
+  }
 
 public:
   Modelling() {
@@ -24,15 +75,16 @@ public:
   ~Modelling() {}
 
   void setCtrlPts(vector<cg::Point2d> ctrlPts) {
-    obj.clear();
+    trianglesObj = 0;
     if (ctrlPts.size() > 1)
-      obj = cg::model(cg::casteljau(ctrlPts));
-    cout << "model size: " << obj.size() << endl;
+      createObj(ctrlPts);
   }
 
   void display() override {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(.0, .0, .2, .0);
+    if (trianglesObj > 0)
+      displayObj();
     glfwSwapBuffers();
   }
 
