eb0626da1610b9d3c5f731a7103ed560747584e7
Rafael Henrique Tibaes <r@fael.nl>
2017-02-09 15:36:44 -0300

    change preview background

--- curveShader.vert
+++ curveShader.vert
@@ -7,6 +7,5 @@ uniform mat4 uMVP;
 
 void main() {
 	vColor = vec4(aColor, 1.0);
-  //gl_Position = uMVP * vec4(aPosition, 1.0, 1.0);
 	gl_Position = vec4(aPosition, 1.0, 1.0);
 }
--- preview.hpp
+++ preview.hpp
@@ -152,7 +152,7 @@ public:
 
   void display() override {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(1.0, 0.0, 0.0, 0.0);
+    glClearColor(.2, .0, .0, .0);
 
     displayLines();
     if (rsz_ctrlPts > 0)
