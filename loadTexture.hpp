2326fbe8391163b9d0310b61bac8832a5e2fad14
Rafael Henrique Tibaes <r@fael.nl>
2017-02-21 16:34:59 -0300

    tried some changes, but no improvement

--- loadTexture.hpp
+++ loadTexture.hpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 #include <GL/glfw.h>
+#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <string>
@@ -9,16 +10,23 @@ using namespace std;
 
 GLuint loadTexture(const string &filename) {
   auto img = imread(filename);
+  cout << "texture size: " << img.size() << endl;
+
   GLuint textureID;
   glGenTextures(1, &textureID);
   glBindTexture(GL_TEXTURE_2D, textureID);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size().width, img.size().height, 0,
-               GL_BGR, GL_UNSIGNED_BYTE, img.data);
+
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, (GLsizei)img.size().width,
+               (GLsizei)img.size().height, 0, GL_BGR, GL_UNSIGNED_BYTE,
+               img.data);
+
   glGenerateMipmap(GL_TEXTURE_2D);
+
   return textureID;
 }
